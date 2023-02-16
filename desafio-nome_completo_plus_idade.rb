77c0e5567d5d772f834c463fe7f6c60452eb131c
Tiago Koakoski <tiago.koakoski@gmail.com>
2023-02-16 11:50:04 -0300

    Add challenge 1

---
+++ desafio-nome_completo_plus_idade.rb
@@ -0,0 +1,12 @@
+# Desafio: Nome completo + idade - Tiago Koakoski - 16/02/2023
+
+puts("Digite seu nome: ")
+name = gets.chomp
+
+puts("Digite seu sobrenome: ")
+lastname = gets.chomp
+
+puts("Digite sua idade: ")
+age = gets.chomp
+
+puts "Olá #{name} #{lastname}, sua idade é #{age} anos"
