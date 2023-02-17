6e152c198515328b4d0ff720a9f8cbdd53468fe4
Tiago Koakoski <tiago.koakoski@gmail.com>
2023-02-16 21:29:35 -0300

    Add challenge-inter2 and 3

---
+++ desafio-intermediario2.rb
@@ -0,0 +1,11 @@
+precoDoGas = gets.to_i
+imposto = 0.1
+impostoCobrado = gets.to_i
+impostoVariavel = gets.to_f
+
+#TODO: Faça o cálculo do preço do gás de acordo com as entradas
+#utilizando estruturas condicionais
+if impostoCobrado == 1
+  imposto = imposto + impostoVariavel/100
+end
+puts "O preço do gás nesse mês é de R$#{(precoDoGas*(1+imposto)).round}"
